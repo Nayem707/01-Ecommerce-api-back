835fbb29bc8a9c75c98c0223833f45299bb9cc18
NAYEM ISLAM <gangsn490@gmail.com>
2024-10-17 00:14:59 -0600

    test-api

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ const dev = require('./config');
 const port = dev.app.port; //set config...
 
 //IMPORT -> ROUTER
-const userRoute = require('./MVC/routers/User_Router');
-const productRoute = require('./MVC/routers/Products_Router');
+// const userRoute = require('./MVC/routers/User_Router');
+// const productRoute = require('./MVC/routers/Products_Router');
 
 //IMPORT -> MIDDLEWARE
 const logger = require('./middleware/logger_middleware');
@@ -30,8 +30,8 @@ app.use(logger);
 app.use(errorHandler);
 
 //ROUTER USES
-app.use('/api/v1/user', userRoute); // USERS
-app.use('/api/v1/product', productRoute); // PRODUCT
+// app.use('/api/v1/user', userRoute); // USERS
+// app.use('/api/v1/product', productRoute); // PRODUCT
 
 // ----- BASIC-SETUP-SHOW-PRODUCTS-IN-TESTING-PURPOSE ------ //
 const products = require('./data/products');
